bc78cbb74356b32cfa5182c4a5ec036a15f7e504
Ilhan Raja <ilhanraja@google.com>
2022-12-31 02:40:46 -0800

    mrk_inject working on macOS 13 Ventura

--- build_cycript_runner.sh
+++ build_cycript_runner.sh
@@ -1,5 +1,11 @@
 set -e
 
-clang -g -arch arm64e -target arm64e-apple-macos -framework AppKit -framework Foundation -shared user/cycript_runner.mm -o build/libcycript_runner.dylib
+clang -g -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -arch arm64 -target arm64-apple-macos -framework AppKit -framework Foundation -shared user/cycript_runner.mm -o build/libcycript_runner_arm64.dylib
 
-codesign -fs - --deep --entitlements entitlements.xml ./build/libcycript_runner.dylib
+clang -g -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -arch arm64e -target arm64e-apple-macos -framework AppKit -framework Foundation -shared user/cycript_runner.mm -o build/libcycript_runner_arm64e.dylib
+
+codesign -fs - --deep --entitlements entitlements.xml ./build/libcycript_runner_arm64.dylib
+
+codesign -fs - --deep --entitlements entitlements.xml ./build/libcycript_runner_arm64.dylib
+
+lipo -create -output build/libcycript_runner.dylib build/libcycript_runner_arm64.dylib build/libcycript_runner_arm64e.dylib
--- user/main.cpp
+++ user/main.cpp
@@ -514,12 +514,14 @@ int main(int argc, char **argv)
 	return 0;
 	*/
 
+	/*
 	using namespace Debug;
 
 	Dwarf *dwarf = new Dwarf("/Library/Developer/KDKs/KDK_12.5_21G72.kdk/System/Library/Kernels/kernel.development.t8020.dSYM/Contents/Resources/DWARF/kernel.development.t8020");
 
 
 	return 0;
+	*/
 
 	while(1)
 	{
